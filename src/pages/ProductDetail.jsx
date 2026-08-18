b875b4ca4fccad96b1e0bd1ef1afb59f00e02196
Bell914 <ittikorn.tipson@gmail.com>
2026-08-18 16:11:54 +0700

    fetch product details by productId

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,3 +1,19 @@
+import { useParams } from "react-router-dom";
+import { products } from "../mock-data/products";
+
 export default function ProductDetail() {
-  return <div>Product Detail</div>;
+  const { productId } = useParams();
+// ดึงข้อมูลของ productId จาก mock data ของ products
+  const product = products.find((p) => p.id === productId);
+
+    if (!product) {
+    return <div>Product not found.</div>;
+  }
+  return (
+    <div>
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p>{product.price}</p>
+    </div>
+  );
 }
