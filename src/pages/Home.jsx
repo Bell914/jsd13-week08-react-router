8f77fedb8a07398720cdc1a5574422623c5d9b77
Bell914 <ittikorn.tipson@gmail.com>
2026-08-18 16:20:06 +0700

    add navigation links to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Welcome to Our App</h1>
+
+      <Link to="/about">
+        <button>Go to About</button>
+      </Link>
+
+      <br />
+      <br />
+
+      <Link to="/contact">Go to Contact</Link>
+    </div>
+  );
 }
