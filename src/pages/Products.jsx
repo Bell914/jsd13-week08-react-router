2b9a15a5546e40479ab55987d717054dbd4b50c5
Bell914 <ittikorn.tipson@gmail.com>
2026-08-18 14:24:51 +0700

    updeta Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,3 +1,16 @@
+import { products } from "../mock-data/products";
+
 export default function Products() {
-  return <div>Products</div>;
+  return (
+    <div className="p-4 bg-white rounded-md shadow-md">
+      <h1 className="text-2xl font-bold mb-4">Products</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+        {products.map((product) => (
+          <div>
+            <h2>{product.name}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
